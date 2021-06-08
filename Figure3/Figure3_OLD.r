cdf000d0f9c1b928b44b377f1b39adb570310b67
Daniel Segre <dsegre@bu.edu>
2021-06-08 16:05:41 -0400

    Updated Figure3.r
    Updated Figure3.r after Reviewers comments

--- Figure3/Figure3_OLD.r
+++ Figure3/Figure3_OLD.r
@@ -3,21 +3,20 @@
 # Author: Demetrius DiMucci
 
 # THE DATA FOR THIS PART CAME FROM HERE: https://ibdmdb.org/tunnel/public/summary.html
-setwd('C:/Users/ddimucci/Desktop/Manuscripts/BowSaw/Figure3/')
+
 
 library(pROC)
 library(randomForest)
-#library(pheatmap)
+library(pheatmap)
 library(PRROC)
-#library(precrec)
 source('Functions.r')
 source('prioritize.r')
 source('kMerFinding.r')
 
 
 ##### LOAD THE DATA AND DO A BIT OF CLEANING TO BE ABLE TO USE IT
- x <- read.csv('hmp2_metadata.csv')
- y <- read.csv('taxonomic_profiles.tsv',sep='\t')
+# x <- read.csv('hmp2_metadata.csv')
+# y <- read.csv('taxonomic_profiles.tsv',sep='\t')
 
 phyl = y[,ncol(y)]
 OTU <- y[,1]
@@ -58,7 +57,6 @@ votes = CDmod$votes[,2]
 g1 = which(pred == 1)
 fg1 = votes[g1]
 bg1 = votes[-g1]
-#auc1 = evalmod(scores = CDmod$votes[,2],labels=CDmod$y)
 roc1 = roc.curve(scores.class0 = fg1,scores.class1 = bg1,curve = TRUE)
 pr1 = pr.curve(scores.class0 = fg1,scores.class1 = bg1,curve = TRUE)
 
@@ -71,24 +69,21 @@ roc2 = roc.curve(scores.class0 = fg1,scores.class1 = bg1,curve = TRUE)
 pr2 = pr.curve(scores.class0 = fg1,scores.class1 = bg1,curve = TRUE)
 
 # Panel A
-jpeg(file='ROCs.jpeg')
 plot(roc1,legend = F,col='black',auc.main = F,main = paste('ROC Curves'),cex.lab=1.5,cex.main=1.5,xlab='1 - Specificity')
 plot(roc2,add = T,col='red')
 
 aucs <- vector(); aucs[1] <- paste('Original:',round(roc1$auc,3));aucs[2] <- paste('Discretized:',round(roc2$auc,3))
 legend('bottomright',legend=aucs,pch=16,col=c(1,2),title='AUC')
 abline(0,1,lty=3)
-dev.off()
 
 # Panel B
-jpeg(file='PRS.jpeg')
 plot(pr1,legend = F,col='black',auc.main = F,main = paste('PR Curves'),cex.lab=1.5,cex.main=1.5)
 plot(pr2,add = T,col='red')
 
 aucs <- vector(); aucs[1] <- paste('Original:',round(pr1$auc.integral,3));aucs[2] <- paste('Discretized:',round(pr2$auc.integral,3))
 abline(h=86/nrow(PRESENCEtable),lty=3)
 legend('bottomright',legend=aucs,pch=16,col=c(1,2),title='AUC')
-dev.off()
+
 
 ### MAKE PANEL C
 ### HOW MANY RULES DO WE NEED TO ACCOUNT FOR EVERYTHING?
@@ -121,11 +116,9 @@ cand3 <- newCand1[1:last,]
 cand2 <- cand2[bor,]
 
 bugRules <- vector()
-bugLengths = vector()
 for(i in 2:7){
   
   small <- which(cand2[,4] < i)
-  
   if(length(small) > 0){
     tempCand = cand3[-small,]
   } else {
@@ -133,8 +126,6 @@ for(i in 2:7){
   }
   
   bugRules <- c(bugRules,kmerFind(candList = tempCand,kmers = i))
-  j = kmerFind(candList = tempCand,kmers = i)
-  bugLengths = c(bugLengths,rep(i,length(j)))
 }
 length(bugRules)
 
@@ -153,7 +144,7 @@ d2 = c2/b1
 
 # PANEL C
 plot(b1,d2,xlab='Number of Exactly Matching Samples',ylab="Precision",cex.lab=1.5)
-abline(h=.9)
+abline(h=.94)
 
 perfs <- which(d2 >= 1)
 bestboi = perfs[which.max(b1[perfs])]
@@ -193,32 +184,9 @@ table(cdonly[leMatch])
 phyl[rule]
 
 
-
-### Display the rules
-for(i in 1:length(best)){
-  j=i
-  rule_val <- as.numeric(strsplit(names(t2)[j],"_")[[1]])
-  rule <- rule_val[seq(1,length(rule_val),2)]
-  vals <- rule_val[seq(1,length(rule_val),2)+1]
-  
-  leMatch <- seq(nrow(PRESENCEtable))
-  for(i in 1:length(rule)){
-    leMatch <- intersect(leMatch,which(PRESENCEtable[,rule[i]]==vals[i]))
-  }
-  tab = table(cdonly[leMatch])
-  
-  phyl[rule]
-  print(phyl[rule])
-  print(c('y','n')[vals])
-  print(paste('************',j,tab[1],tab[2]))
-}
-
 ########### Check how the aggregate rules behave
 # Obtain results for table 2
 # I Manually wrote these into the table
-jpeg()
-plot(b1,d2,xlab='Number of Exactly Matching Samples',ylab="Precision",cex.lab=1.5)
-abline(h=.9)
 allMatch <- vector()
 for(j in 1:length(t2)){
   rule_val <- as.numeric(strsplit(names(t2)[j],"_")[[1]])
@@ -229,63 +197,14 @@ for(j in 1:length(t2)){
   for(i in 1:length(rule)){
     leMatch <- intersect(leMatch,which(PRESENCEtable[,rule[i]]==vals[i]))
   }
-  
-  hits = length(leMatch)
-  prec = length(which(CDmodBinary$y[leMatch] == 1))/hits
-  
-  points(hits,prec,col=2,pch=16,cex=1.5)
-  legend('bottomright',legend=c('Selected Sub-Rule'),col=2,pch=16)
   print(table(cdonly[leMatch]))
   #print(phyl[rule])
   #print(table(cdonly[allMatch]))
   allMatch <- union(allMatch,leMatch)
 }
-dev.off()
 table(cdonly[allMatch])
 table(diagnosis[allMatch])
 
 labs2 = rep(0,length(cdonly))
 labs2[allMatch]=1
 auc(cdonly,labs2)
-
-
-##########
-isparent = vector()
-parents = rowSums(sign(cand3))
-parlength = vector()
-chillength = vector()
-for(i in 1:length(best)){
-  r = names(bug3[best[i]])
-  r = gsub(pattern = '_1_',replacement = ' ',x = r)
-  r = gsub(pattern = '_2_',replacement = ' ',x = r)
-  r = gsub(pattern = '_1',replacement = ' ',x = r)
-  r = gsub(pattern = '_2',replacement = ' ',x = r)
-  r = as.numeric(strsplit(x = r,split = ' ')[[1]])
-  
-  childof = which(rowSums(sign(cand3[,r])) == length(r))
-  print(childof)
-  parlength[i] = parents[childof]
-  chillength[i] = length(r)
-  isparent[i] = length(r) %in% parents[childof]
-}
-summary(chillength)
-summary(parlength)
-
-
-
-
-################## If you're curious about how logistic regression does on this data set
-B = (OTUtable)
-labs = CDmod$y
-
-
-train = sample(nrow(B),90)
-B1 = data.frame(B[train,])
-L1 = labs[train]
-B2 = B[-train,]
-train.df=data.frame(y=L1,B1)
-mood=glm(y~.,train.df,family=binomial('logit'))
-test.df = data.frame(y=labs[-train],B2)
-pred1 = predict(mood,test.df)
-plot(evalmod(scores=pred1,labels = labs[-train]))
-table(sign(pred1),labs[-train])
